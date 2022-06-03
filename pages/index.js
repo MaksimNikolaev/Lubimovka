43be7bb3d8dee318404b2ae06911662b1a59809d
softlolx <softlolx@gmail.com>
2022-06-03 18:01:14 +0300

    fix: file structure

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const headerSiteNav = document.querySelector(".header__nav");
 const burgerButton = document.querySelector(".header__burger-menu");
 
 function toggleSiteNav() {
-  headerSiteNav.classList.toggle("header__site-nav_active");
+  headerSiteNav.classList.toggle("header__nav_active");
 }
 
 burgerButton.addEventListener("click", toggleSiteNav);
