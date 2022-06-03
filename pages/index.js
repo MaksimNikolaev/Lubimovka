e3d3cf96df68f6d8384cc653450d2363cd4441c8
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-06-03 14:51:56 +0400

    feat: add photo-grid

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+const PopupPhoto = document.querySelector(".popup-photo");
+const PhotoGridImage = document.querySelector(".photo-grid__image");
+const PhotoGridList = document.querySelector(".photo-grid__list");
+
+function openPopup() {
+  PopupPhoto.classList.add("popup-photo_opened");
+}
+
+function closePopup() {
+  PopupPhoto.classList.remove("popup-photo_opened");
+}
+
+function addPhotoInPopup(element) {
+  PopupPhoto.querySelector(".popup-photo__item").src = element.src;
+}
+
+PhotoGridList.addEventListener("click", (evt) => {
+  addPhotoInPopup(evt.target);
+  openPopup();
+});
+PopupPhoto.addEventListener("click", () => closePopup());
