99aba90bc8c43346d9938c039627abbaa49f776b
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-06-18 16:26:50 +0300

    feat: add slider to block feedback

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,54 +1,117 @@
-const slides = document.querySelectorAll('.review__item');
-const dots = document.querySelectorAll('.slider__dots-item');
-const prevButton = document.querySelector('.slider__button_type_prev');
-const nextButton = document.querySelector('.slider__button_type_next');
-let slideIndex = 1;
+const reviewSlides = document.querySelectorAll('.review__item');
+const reviewDots = document.querySelector('.review__dots').querySelectorAll('.slider__dots-item');
+const reviewPrevButton = document.querySelector('.review__buttons').querySelector('.slider__button_type_prev');
+const reviewNextButton = document.querySelector('.review__buttons').querySelector('.slider__button_type_next');
+const reviewActiveClass = 'review__item_active';
+const reviewPrevClass = 'review__item_prev';
+let reviewSlideIndex = 1;
+const feedbackSlides = document.querySelectorAll('.feedback__item');
+const feedbackDots = document.querySelectorAll('.feedback__dots-item');
+const feedbackPrevButton = document.querySelector('.feedback__buttons').querySelector('.slider__button_type_prev');
+const feedbackNextButton = document.querySelector('.feedback__buttons').querySelector('.slider__button_type_next');
+const feedbackActiveClass = 'feedback__item_active';
+const feedbackPrevClass = 'feedback__item_prev';
+let feedbackSlideIndex = 1;
 
-function showSlides(n) {
+function removeClasses(slides, dots, active, prev) {
+  Array.from(slides).forEach((item) => {
+    item.classList.remove(active);
+    item.classList.remove(prev);
+    if(item.classList.contains('review__item')) {
+      item.style.visibility = 'hidden';
+    }
+    if(item.classList.contains('feedback__item')) {
+      item.style.order = 1;
+    }
+  });
+  Array.from(dots).forEach((item) => {
+    item.classList.remove('slider__dots-item_active');
+  });
+}
+
+function addSlidesClasses(current, active) {
+  current.classList.add(active);
+}
+
+function addDotsActiveClass(dot) {
+  dot.classList.add('slider__dots-item_active');
+}
+
+function changeVisibility(current, next, slides, n, prev, prevEl) {
+  current.style.visibility = 'visible';
+  if (n < slides.length) {
+    next.style.visibility = 'visible';
+  }
+  if (n > 1) {
+    prevEl.style.visibility = 'visible';
+    prevEl.classList.add(prev);
+  }
+}
+
+function changeOrder(slides, current) {
+  let index = Array.from(slides).indexOf(current);
+  for(let i =0; i < index; i++) {
+    slides[i].style.order = 2;
+  }
+}
+
+function showSlides(n, slides, dots, active, prev) {
   const currentSlide = slides[n-1];
   const nextSlide = slides[n];
   const prevSlide = slides[n-2];
   const activeDot = dots[n-1];
 
-  Array.from(slides).forEach((item) => {
-    item.classList.remove('review__item_active');
-    item.classList.remove('review__item_prev');
-    item.style.visibility = 'hidden';
-  });
-  Array.from(dots).forEach((item) => {
-    item.classList.remove('slider__dots-item_active');
-  });
-
-  currentSlide.classList.add('review__item_active');
-  currentSlide.style.visibility = 'visible';
-  if (n < slides.length) {
-    nextSlide.style.visibility = 'visible';
+  removeClasses(slides, dots, active, prev);
+  addSlidesClasses(currentSlide, active);
+  addDotsActiveClass(activeDot);
+  if(currentSlide.classList.contains('review__item')) {
+    changeVisibility(currentSlide, nextSlide, slides, n, reviewPrevClass, prevSlide);
   }
-  if (n > 1) {
-    prevSlide.style.visibility = 'visible';
-    prevSlide.classList.add('review__item_prev');
+  if(currentSlide.classList.contains('feedback__item')) {
+    changeOrder(slides, currentSlide);
   }
-  activeDot.classList.add('slider__dots-item_active');
 }
 
 function plusSlide() {
-  if(slideIndex < slides.length) {
-    showSlides(slideIndex += 1);
+  if(reviewSlideIndex < reviewSlides.length) {
+    showSlides(reviewSlideIndex += 1, reviewSlides, reviewDots, reviewActiveClass, reviewPrevClass);
   }
 }
 
 function minusSlide() {
-  if(slideIndex > 1) {
-    showSlides(slideIndex -= 1);
+  if(reviewSlideIndex > 1) {
+    showSlides(reviewSlideIndex -= 1, reviewSlides, reviewDots, reviewActiveClass, reviewPrevClass);
   }
 }
 
-for (let i=0; i < dots.length; i++) {
-  dots[i].addEventListener('click', function() {
-    showSlides(i+1);
+function plusFeedbackSlide() {
+  if(feedbackSlideIndex < feedbackSlides.length) {
+    showSlides(feedbackSlideIndex += 1, feedbackSlides, feedbackDots, feedbackActiveClass, feedbackPrevClass);
+  }
+}
+
+function minusFeedbackSlide() {
+  if(feedbackSlideIndex > 1) {
+    showSlides(feedbackSlideIndex -= 1, feedbackSlides, feedbackDots, feedbackActiveClass, feedbackPrevClass);
+  }
+}
+
+for (let j = 0; j < reviewDots.length; j++) {
+  reviewDots[j].addEventListener('click', function() {
+    showSlides(j + 1, reviewSlides, reviewDots, reviewActiveClass, reviewPrevClass);
   });
 }
-prevButton.addEventListener('click', minusSlide);
-nextButton.addEventListener('click', plusSlide);
 
-showSlides(slideIndex);
+for (let k = 0; k < feedbackDots.length; k++) {
+  feedbackDots[k].addEventListener('click', function() {
+    showSlides(k + 1, feedbackSlides, feedbackDots, feedbackActiveClass, feedbackPrevClass);
+  });
+}
+
+reviewPrevButton.addEventListener('click', minusSlide);
+reviewNextButton.addEventListener('click', plusSlide);
+feedbackPrevButton.addEventListener('click', minusFeedbackSlide);
+feedbackNextButton.addEventListener('click', plusFeedbackSlide);
+
+showSlides(reviewSlideIndex, reviewSlides, reviewDots, reviewActiveClass, reviewPrevClass);
+showSlides(feedbackSlideIndex, feedbackSlides, feedbackDots, feedbackActiveClass, feedbackPrevClass);
